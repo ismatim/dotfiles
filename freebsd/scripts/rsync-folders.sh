c5cb92d51e94b2c0f3afbe16f0a4b4b9c36449f3
Ismael J. Tisminetzky <me@ismaeltisminetzky.xyz>
2022-05-03 16:15:26 -0300

    minor fix

--- freebsd/scripts/rsync-folders.sh
+++ freebsd/scripts/rsync-folders.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+
+# osx
 rsync -ahtrzvvv -e ssh  ismaeltisminetzky@cygnus:/Users/ismaeltisminetzky/UNIT_DATA/ebooks/ /tank_data_1/ebook/
 rsync -ahtrzvvv -e ssh  ismaeltisminetzky@cygnus:/Users/ismaeltisminetzky/UNIT_DATA/personal/ /tank_data_1/personal/
 rsync -ahtrzvvv -e ssh ismaeltisminetzky@cygnus:/Users/ismaeltisminetzky/Pictures/ /tank_data_1/photos/
+
+# www.ismaeltisminetzky.xyz
 rsync -ahtrzvvv -e ssh root@ismaeltisminetzky.xyz:/etc/ /tank_data_1/projects/openbsd/etc/
 rsync -ahtrzvvv -e ssh root@ismaeltisminetzky.xyz:/root/dotfiles/ /tank_data_1/projects/openbsd/dotfiles/
 rsync -ahtrzvvv -e ssh root@ismaeltisminetzky.xyz:/home/me/Maildir/ /tank_data_1/projects/openbsd/Maildir/
